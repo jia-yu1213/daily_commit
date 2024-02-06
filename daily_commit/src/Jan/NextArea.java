9109688152c13fe75adc83375d34128a235f7f5b
Jia Yu <yujia1213@naver.com>
2024-02-06 12:43:16 +0900

    [프로그래머스] Lv1 > pcce 기출문제 9번 > 이웃한칸

---
+++ daily_commit/src/Jan/NextArea.java
@@ -0,0 +1,31 @@
+package Jan;
+
+public class NextArea {
+
+    public static void main(String[] args) {
+        String[][] board = {{"blue", "red", "orange", "red"}, {"red", "red", "blue", "orange"}, {"blue", "orange", "red", "red"}, {"orange", "orange", "red", "blue"}};
+        int h = 1;
+        int w = 1;
+
+        int answer = solution(board,h,w);
+        System.out.println("answer = " + answer);
+    }
+
+    public static int solution(String[][] board, int h, int w) {
+        int answer = 0;
+        String color = board[h][w];
+        int[] dh = {0, 1, -1, 0};
+        int[] dw = {1, 0, 0, -1};
+
+        for (int i = 0; i < 4; i++) {
+            int h_check = h + dh[i];
+            int w_check = w + dw[i];
+            if (0 <= h_check && w_check >= 0 && h_check < board.length && w_check < board[0].length
+                    && color.equalsIgnoreCase(board[h_check][w_check])){
+                answer += 1;
+            }
+        }
+
+        return answer;
+    }
+}
